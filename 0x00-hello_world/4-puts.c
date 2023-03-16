89e87195701501c1207b843a837f22d27269f9df
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-16 08:44:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*
+ * the main: the entry point in C
+ * return 0: when main returns,the program stops
+ * puts: to write a line or string to the output stream ( stdout ) that is up to
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
