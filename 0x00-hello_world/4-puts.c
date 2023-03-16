5a0ad636e24f9824fd5790a91ac7f16c379daa08
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-16 09:39:07 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main -  the entry point in C
  *
- * Description:the program contains a puts function that writes a line or string
+ * Description:the program has  a puts function that writes a line or string
  * Return: 0 that means there are no errors
  */
 int main(void)
