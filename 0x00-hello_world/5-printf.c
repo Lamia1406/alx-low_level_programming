59f1d9eaa631d4574c073cf6d1277638e8a0214c
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-16 09:44:03 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  the entry point in C
+ *
+ * Description:the program has  a printf that prints an output
+ * Return: 0 that means there are no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
