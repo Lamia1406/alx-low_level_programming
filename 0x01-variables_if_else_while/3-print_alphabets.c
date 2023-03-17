85a9407c2a8f43636c6d115a165ec1f251df2885
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-17 05:57:39 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - entry point of the program
+ *Description: this program prints alphabet letter by letter
+ *Return: 0 successful operation
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
