2eadf640cd48bdc0514ee3619e4fc20ada94b759
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-21 16:06:12 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point of the program
+ *Description: this program prints _putchar
+ *Return: 0 successful operation
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
