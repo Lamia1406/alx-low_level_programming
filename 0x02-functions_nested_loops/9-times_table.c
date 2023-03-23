e7d632ae0d9ae6ba0f7e2b2b2a6a0abc0ad3a472
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-22 19:17:38 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 	{
 		for (times = 0 ; times < 10 ; times++)
 		{
-			printf("%*d", times * a < 0 ? 2  : 3, times * a);
+			printf("%2d", times * a);
 			if (times < 9)
-				printf(",");
+				printf(", ");
 		}
 		times = 0;
 		printf("\n");
