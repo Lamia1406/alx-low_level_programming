197035296cc271ed982f599cc05dacb413ae5817
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-22 17:48:30 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - entry point of the progrm
+ *@a: the user input first operand
+ *@b: the user input second operand
+ *Description: this program adds two numbers
+ *Return: int result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
