ff93b3f45ed63019c780a30b5d88b9b6490407c6
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-24 10:12:07 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * FizzBuzz - entry point of the program
+ * Description:	this program prints numbers from 1 to 100 except for:
+ *		multiplies of 3 and 5 (prints FizzBuzz)
+ *		multiplies of only 3 (prints Fizz)
+ *		multiplies of only 5 (prints Buzz)
+ *
+ * Return: void
+ */
+void FizzBuzz(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+}
