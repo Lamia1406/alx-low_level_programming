658c82ea2b4addee9e5a6aaea4d2a7dc465ab128
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-27 14:19:17 -0700

    update variable via pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - entry point of the program
+ * @c: the input character
+ * Description: this program returns if a letter is uppercase or not
+ * Return: 1 or 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
