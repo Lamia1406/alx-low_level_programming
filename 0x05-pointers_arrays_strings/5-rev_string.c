1f32a753040a7e637fa1499ef08417890c0add48
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-28 01:56:18 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - entry point of the program
+ * @s: the input string
+ * Description: this program reverses a string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	char rev[strlen(s)];
+
+	for (i = 0 ; i < strlen(s) ; i++)
+	{
+		rev[strlen(s) - 1 - i] = s[i];
+	}
+	for (i = 0 ; i < strlen(s) ; i++)
+	{
+		s[i] = rev[i];
+	}
+}
+
