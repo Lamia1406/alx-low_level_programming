425f982ad11e3c71f1db74328fa9edd952dc2608
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-28 01:25:51 -0700

    return length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - entry point of the program
+ * @s: the input string
+ * Description: this program returns the length of a string
+ * Return: int (length)
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
