db8fd2d24b9d620ab409f777259aa76d17d4779f
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-28 01:29:37 -0700

    print string with puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - entry point of the program
+ * @str: the input string
+ * Description: this program prints a string using puts
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
