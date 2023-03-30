dcbc3515bfaccc9021d0e2b35bee3a33597305e2
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-30 12:23:34 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcat - entry point of the program
+ * @dest: the first string
+ * @srch: the second string
+ * Description: this program concatenates two strings
+ * Return: char
+ */
+char *_strcat(char *dest, char *srch)
+{
+	strcat(dest, srch);
+	return (dest);
+
+
+}
+
