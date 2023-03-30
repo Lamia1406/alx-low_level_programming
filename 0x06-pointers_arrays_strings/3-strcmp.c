5143585e66789946f68b6183c8fcf35fe40ba6b6
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-30 14:56:47 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcmp - entry point of the program
+ * @s1: the first string
+ * @s2: the second string
+ * Description: this program compares two strings
+ * Return: int (1 or 0)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #include<string.h>
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
