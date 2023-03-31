55a7852d906afb0d5932fdcecfa0849ef754666e
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-30 20:11:37 -0700

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - entry point of the program
+ * @a: the input array
+ * @n: the length of an array
+ * Description: this program reverses an array
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0 ; i <  n ; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
