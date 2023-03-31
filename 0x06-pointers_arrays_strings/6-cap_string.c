cde71aa87075b855bec8e869973c31d22b7ccb2e
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-03-31 07:01:25 -0700

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - entry point of the program
+ * @str: the input string
+ * Description: this program capitalizes the words
+ * Return: char
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char separator[] = ",;.!?\"(){}\n\t ";
+
+	for (i = 0 ; str[i] ; i++)
+	{
+		for (j = 0 ; separator[j] ; j++)
+		{
+			if (str[i] == separator[j] && str[i + 1])
+				str[i + 1] = toupper(str[i + 1]);
+		}
+	}
+	str[i] = '\0';
+
+	return (str);
+}
