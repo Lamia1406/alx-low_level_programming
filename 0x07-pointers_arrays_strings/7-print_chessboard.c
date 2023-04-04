fcd87d29456a05fdb555bee49852aa55a258a083
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-04 08:59:20 -0700

    print 2D array

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - entry point of the program
+ * @a: pointer to the array
+ * Description: this function prints a 2D array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0 ; a[i] ; i++)
+	{
+		for (j = 0; j < 8 ; j++)
+		{
+			if (a[i][j] == '\0')
+				break;
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
