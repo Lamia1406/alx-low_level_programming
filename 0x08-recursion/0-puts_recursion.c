803d2d89207098d256968fc86f3c426655b674bb
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-05 12:12:52 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - entry point of the program
+ * @s: the string input
+ * Description: this program prints a string
+ * Return: char
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
