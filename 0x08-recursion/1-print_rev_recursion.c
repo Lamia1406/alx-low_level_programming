185107023629f659ec0693803df6e51f9aadf759
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-06 15:54:30 -0700

    print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _print_rev_recursion- entry point of the program
+ * @s: the string input
+ * Description: this program prints the reverse of a string
+ * Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+	void printc(char *c, int i);
+	int i = strlen(s);
+
+	printc(s, i);
+}
+/**
+ * printc-prints string chars in reverse
+ * @c: string input
+ * @i: length of the string
+ * Return: void
+ */
+void printc(char *c, int i)
+{
+	if (i < 0)
+	{
+		return;
+	}
+	else
+	{
+		printf("%c", c[i]);
+		printc(c, --i);
+	}
+}
