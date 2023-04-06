5174b5c7a873b90e8c428c3e9e2c09aa42d53aec
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-06 16:28:07 -0700

    return length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strlen_recursion- entry point of the program
+ * @s: the string input
+ * Description: this program prints the reverse of a string
+ * Return: void
+*/
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
