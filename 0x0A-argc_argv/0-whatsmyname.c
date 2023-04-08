a5638060416003cf6acb2419a01f0c279c5a88f6
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-08 06:40:44 -0700

    print the program's name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * Description: this program prints its name, followed by a new line.
  * Return: int
 */
-int main(int argc, char *argv[])
+int  main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
