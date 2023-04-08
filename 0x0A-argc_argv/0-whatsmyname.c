a70a608a10d8487266568cdec2fd286798c39afd
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-08 06:21:13 -0700

    print the program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main- entry point of the program
+ * @argc: the number of command-line arguments
+ * @argv: the array containing command-line arguments
+ * Description: this program prints its name, followed by a new line.
+ * Return: int
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
