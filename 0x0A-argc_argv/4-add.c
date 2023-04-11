18ad1cdd162491595a262d22d6056e05bdd3a0ff
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-10 18:26:57 -0700

    add only positive numbers in argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- entry point of the program
+ * @argc: the number of command-line arguments
+ * @argv: the array containing command-line arguments
+ * Description: this program prints its name, followed by a new line.
+ * Return: int
+*/
+int  main(int argc, char *argv[])
+{
+	int i;
+	int s = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				if (atoi(argv[i]) >= 0)
+					s = s + atoi(argv[i]);
+			}
+		}
+		printf("%d\n", s);
+		return (0);
+	}
+}
