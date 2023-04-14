7a21356239b8b6d8fecab0188ce5c49d52a8fee7
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-14 14:26:13 -0700

    concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
-* string_nconcat - the entry point of the program
-* @s1: the first string input
-* @s2: the second string input
-* @n :the number of bytes to copy from s2 to s1
-* Description : this function concatenates two strings
-* Return: char
-*/
+ * string_nconcat - concatenate two stirngs
+ * @s1: first string
+ * @s2: second string
+ * @n: n char to concatenate
+ *
+ * Return: pointer to the new str
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int concatLen;
-	char *ptr;
-	int lenS1;
-	int lenS2;
-	int i, j;
+	unsigned int len1, len2, i, j;
+	char *str;
 
 	if (s1 == NULL)
-		lenS1 = 0;
+		len1 = 0;
 	else
-		for (lenS1 = 0 ; s1[lenS1] != '\0' ; lenS1++)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
 			;
+	}
 	if (s2 == NULL)
-		lenS2 = 0;
+		len2 = 0;
 	else
-		for (lenS2 = 0 ; s2[lenS2] != '\0' ; lenS2++)
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
 			;
-	if (n > lenS2)
-		n = lenS2;
-	ptr = malloc((lenS1 + n + 1) * sizeof(char));
+	}
 
-	if (ptr == NULL)
+	if (n > len2)
+		n = len2;
+	str = malloc(sizeof(char) * (len1 + n + 1));
+	if (str == NULL)
 		return (NULL);
-	for (i = 0 ; i < lenS1 ; i++)
-		ptr[i] = s1[i];
-	for (j = 0 ; j < n ; i++, j++)
-		ptr[i] = s2[j];
-	ptr[lenS1 + n] = '\0';
-	return (ptr);
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		str[i] = s2[j];
+	str[len1 + n] = '\0';
+	return (str);
 }
