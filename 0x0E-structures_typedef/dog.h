7694dce6a6e06436240186abcd5f91738b814b11
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-07-18 15:38:10 -0700

    create simple struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - dog identity card
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 
 struct dog
 {
 	char *name;
 	float age;
+
 	char *owner;
 };
 
