2fd849f70943b289c5ef47c32acb6dc17539280f
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-08-04 00:49:36 -0700

    fix clear_bit function

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
 		return (-1);
-	if (index != 0 && *n > (unsigned long int)(1 << index))
+	if (index != 0 && *n >= (unsigned long int)(1 << index))
 		*n = *n ^ (1 << index);
 	return (1);
 }
