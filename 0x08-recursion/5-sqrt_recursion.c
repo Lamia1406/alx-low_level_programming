716fbf0ce361b6dc443ed2e92e896e7cc3b0d508
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-23 02:11:10 -0700

    compute the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _sqrt_recursion- entry point of the program
+ * @n: The number for which to calculate the square root.
+ * Return: The square root of n if it's a perfect square, -1 otherwise.
+*/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, n));
+}
+/**
+ * _sqrt - Computes the square root of a number using recursion.
+ * @n: The number for which to calculate the square root.
+ * @i: The current integer to check as a possible square root.
+ * Return: The square root of n if it's a perfect square, -1 otherwise.
+ */
+int _sqrt(int n, int i)
+{
+	if (i == 0 || n < 0)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	_sqrt(n, i - 1);
+}
