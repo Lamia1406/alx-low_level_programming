a5a1157d9c5d8cd787d5491b8d55f7e31e7c9df2
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-27 09:42:27 -0700

    fix bugs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,9 +14,12 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			printf("%d", i * j);
+			if (j == 0)
+				printf("%d", i * j);
+			else
+				printf("%3d", i * j);
 			if (j != n)
-				printf(",   ");
+				printf(", ");
 		}
 		printf("\n");
 	}
